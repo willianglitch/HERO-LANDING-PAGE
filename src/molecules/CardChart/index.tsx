794333a87a523bbd56b003cf56ff66b0af7fe751
willianglitch <nailliw901@gmail.com>
2022-12-06 21:21:41 -0300

    creating chart columns

--- src/molecules/CardChart/index.tsx
+++ src/molecules/CardChart/index.tsx
@@ -1,9 +1,23 @@
 import * as S from './styles'
+import FirstArrow from '../../assets/FirstArrow.svg'
+import SecondArrow from '../../assets/SecondArrow.svg'
 
 export const CardChart = () => {
+
+    const amountBars = Array.from(Array(12).keys())
+
     return(
         <S.Container>
-            
+            <div>
+                <img src={FirstArrow} alt="icone de uma seta esquerda" />
+                <strong>starts</strong>
+                <img src={SecondArrow} alt="icone de uma seta direita" />
+            </div>
+
+            <S.Chart>
+                {amountBars.map((item) => (<div><span></span></div>))}
+            </S.Chart>
+
         </S.Container>
     )
 }
--- src/molecules/CardChart/styles.ts
+++ src/molecules/CardChart/styles.ts
@@ -10,4 +10,41 @@ export const Container = styled.div`
     box-shadow: 0px 17.5px 28px rgba(0,0,0,0.1);
     bottom: 10px;
     left: 40px;
+    padding: 33.6px 24.5px;
+
+    >div{
+        display: flex;
+        justify-content: space-between;
+        text-align: center;
+    }
+    >div strong{
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    >div img{
+        height: 18px;
+    }
+`
+
+
+export const Chart = styled.div`
+    margin-top: 28px;
+
+    div {
+        height: 91px;
+        width: 3px;
+        background: #EFF3FE;
+        border-radius: 4px;
+        display: flex;
+        align-items: flex-end;
+
+        span{
+            background: #CC914F;
+            display: block;
+            height: 45px;
+            width: 3.8px;
+            border-radius: 5px;
+        }
+    }
 `
